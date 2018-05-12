149976a533010f76a9be1d5c83a83966e8435746
Jefson Alves <jefson.s.a7@gmail.com>
2018-05-12 08:49:00 -0300

    atualização do dia 12/05 template

---
+++ src/main/java/media/imagesView.java
@@ -0,0 +1,26 @@
+package media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class imagesView {
+	
+	private List<String> images;
+	
+	@PostConstruct
+	public void init(){
+		
+		images = new ArrayList<String>();
+		for (int i = 1; i <= 12; i++){
+			images.add("nature" + i + ".jpg");
+		}
+	}
+	public List<String> getImages(){
+		return images;
+	}
+	
+}
