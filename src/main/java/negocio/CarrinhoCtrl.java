cb9d8444445567703f599c591a06429fbfe1b6c7
JefsonAlves <jefson.s.a@hotmail.com>
2018-06-07 20:40:24 -0300

    novas atualizações

--- src/main/java/negocio/CarrinhoCtrl.java
+++ src/main/java/negocio/CarrinhoCtrl.java
@@ -5,54 +5,43 @@ import java.util.List;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-
+import javax.faces.bean.SessionScoped;
 import beans.*;
+import negocio.*;
 
 @ManagedBean
 @ApplicationScoped
 public class CarrinhoCtrl {
-	private Pedido pedido = new Pedido();
-
 	private Produto prod = new Produto();
-	private List<Produto> listaProdutos = new ArrayList<Produto>();
-
-	public void addProduto() {
-		listaProdutos.add(prod);
-		this.prod = new Produto();
-	}
-
-	public Produto getProd() {
-		return prod;
-	}
-
-	public void setProd(Produto prod) {
-		this.prod = prod;
-	}
+	private Pedido pedido = new Pedido();
+	private List<Produto> listaProduto = new ArrayList<Produto>();
 
 	public List<Produto> getListaProdutos() {
-		return listaProdutos;
+		return listaProduto;
 	}
 
 	public void setListaProdutos(List<Produto> listaProdutos) {
-		this.listaProdutos = listaProdutos;
+		this.listaProduto = listaProdutos;
 	}
 
 	public Pedido getPedido() {
 		return pedido;
-	}
+		}
 
 	public void setPedido(Pedido pedido) {
 		this.pedido = pedido;
 	}
-	 public void addCarrinho(Produto produto) {
-		 Itens_pedido addcarrinho = new Itens_pedido();
-		 
-		 addcarrinho.setPro_id(produto);
-		 addcarrinho.setIpe_qtde(1);
-		 addcarrinho.setIpe_valorunit(produto.getPreco()*1);
-		 addcarrinho.setIpe_subtotal(produto.getPreco()*1);
-		 addcarrinho.setPed_id(pedido);
-		 
-		 pedido.getPedidos().add(addcarrinho);
-	 }
+
+	public void addCarrinho(Produto produto) {
+		Itens_pedido addcarrinho = new Itens_pedido();
+		listaProduto.add(prod);
+		this.prod = new Produto();
+		addcarrinho.setPro_id(produto);
+		addcarrinho.setIpe_qtde(1);
+		addcarrinho.setIpe_valorunit(produto.getPreco() * 1);
+		addcarrinho.setIpe_subtotal(produto.getPreco() * 1);
+		addcarrinho.setPed_id(pedido);
+
+		pedido.getPedidos().add(addcarrinho);
+	}
 }
